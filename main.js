0ad1189cb3487f837c49fddc358099551df91764
Lila <djyasin.ly@gmail.com>
2021-10-07 13:58:52 -0400

    Got decimal working.

--- main.js
+++ main.js
@@ -34,35 +34,6 @@ clear.addEventListener('click', () => {
     location.reload()
  })
 
- //decimal.addEventListener('click', () => {
-  //  display.innerText += decimal(display.innerText)
- // if (calculator.displayValue.includes(decimal)) {
-  //  Append the decimal point
-// calculator.displayValue += decimal;
-//}
-//}
- //})
-
-//function inputDecimal(decimal) {
-//If the 'displayValue' property does not contain a decimal point
- //  if (calculator.displayValue.includes(decimal)) {
-  //      Append the decimal point
- //    calculator.displayValue += decimal;
- //   }
-  //}
-
- //if (target.classList.contains('decimal')) {
- //   console.log('decimal', target.value);
-//    return;
- // }
- //function Dot()                  //PUT IN "." if appropriate.
-// {
- //   if ( Current.length == 0)     //no leading ".", use "0."
- //     { Current = "0.";
-  //    } else
-  //    {  if ( Current.indexOf(".") == -1)
-  //         { Current = Current + ".";
-  //    };   };
-  //  document.Calculator.Display.value = Current;
- //  }
-  
+decimal.addEventListener('click', () => {
+        display.innerText += decimal.innerText
+      })
